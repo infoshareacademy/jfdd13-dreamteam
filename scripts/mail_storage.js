72789863bbaf4233da57a0e074974ef8169e10ad
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-18 19:28:41 +0200

    added thank you info  after submit

--- scripts/mail_storage.js
+++ scripts/mail_storage.js
@@ -1,9 +1,11 @@
 const
     inputMail = document.querySelector("#input__form"),
-    submitMail = document.querySelector("#form__submit");
-
+    submitMail = document.querySelector("#form__submit"),
+    thankYou = document.querySelector("#thankYou");
+    
 function addMail(){
     localStorage.setItem("email", inputMail.value);
+    thankYou.innerText = 'Dziękujemy! Zapewniamy o bezpieczeństwie Państwa danych.';
     event.preventDefault();
 }
 
