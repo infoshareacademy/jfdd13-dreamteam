27c60f349bef6e4aeb3c96a9d92e219678f50ccd
rednaxela700 <ohleg@windowslive.com>
2019-10-21 16:00:43 +0200

    final fixes and remember: don't commit to master

--- scripts/mail_storage.js
+++ scripts/mail_storage.js
@@ -1,5 +1,7 @@
 const
     inputMail = document.querySelector("#form__input"),
+    checkbox = document.querySelector('input[type=checkbox]'),
+    btn = document.querySelector('button'),
     submitMail = document.querySelector("#form__submit"),
     thankYou = document.querySelector("#thankYou");
     
@@ -17,6 +19,7 @@ window.addEventListener('scroll', function(e) {
     console.log(window.scrollY);
     if (window.scrollY > 140) {
         document.querySelector('nav').classList.add('scrolled');
+        // document.querySelector('.show_menu').classList.add('scrolled');
     } else{
         document.querySelector('nav').classList.remove('scrolled');
     }
