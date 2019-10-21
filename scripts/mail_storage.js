00cf10b7ba7b2cef207a53c676d12630b93a6541
rednaxela700 <ohleg@windowslive.com>
2019-10-21 20:19:15 +0200

    fixes, major fixes...
    - put nav scroll to another js file
    - deleted text-transform in h3 headings
    - minor fixes in mail_storage.js

--- scripts/mail_storage.js
+++ scripts/mail_storage.js
@@ -1,41 +1,28 @@
 const
     inputMail = document.querySelector("#form__input"),
-    checkbox = document.querySelector('input[type=checkbox]'),
+    checkbox = document.querySelector('input[type="checkbox"]'),
     btn = document.querySelector('button'),
     submitMail = document.querySelector("#form__submit"),
-    thankYou = document.querySelector("#thankYou"),
-    isCheck = document.querySelector("#check__input");
-    
+    thankYou = document.querySelector("#thankYou");
+
 function addMail(){
-    if(inputMail.value.length !== 0){
+    if(inputMail.value.length !== 0) {
         localStorage.setItem("email", inputMail.value);
         thankYou.classList.add('form__red--no');
         thankYou.innerText = 'Dziękujemy! Zapewniamy o bezpieczeństwie Państwa danych.';
-    } else{
+    } else {
         thankYou.classList.add('form__red');
-        thankYou.innerText = 'Prosimy o podanie adres e-mail.';
+        thankYou.innerText = 'Prosimy o podanie adresu e-mail.';
     }     
 }
 function givePerm(){
     event.preventDefault();
-    if (isCheck.checked == true){
+    if (checkbox.checked === true){
         addMail();
-    }else{
+    } else {
         thankYou.classList.add('form__red');
         thankYou.innerText = 'Prosimy o wyrażenie zgody na przetwarzanie danych osobowych.';
     }
 }
 
 submitMail.addEventListener("click", givePerm);
-
-//nav animation onscroll
-
-window.addEventListener('scroll', function(e) {
-    console.log(window.scrollY);
-    if (window.scrollY > 140) {
-        document.querySelector('nav').classList.add('scrolled');
-        // document.querySelector('.show_menu').classList.add('scrolled');
-    } else{
-        document.querySelector('nav').classList.remove('scrolled');
-    }
-});
---
+++ scripts/navscroll.js
@@ -0,0 +1,11 @@
+//nav animation onscroll
+
+window.addEventListener('scroll', function(e) {
+    // console.log(window.scrollY);
+    if (window.scrollY > 140) {
+        document.querySelector('nav').classList.add('scrolled');
+        // document.querySelector('.show_menu').classList.add('scrolled');
+    } else{
+        document.querySelector('nav').classList.remove('scrolled');
+    }
+});
