c309bee31db97b1a5d148f324565e80bca80a1e8
rednaxela700 <ohleg@windowslive.com>
2019-10-27 13:02:27 +0100

    put nav animation js into seperate function

--- scripts/navscroll.js
+++ scripts/navscroll.js
@@ -3,25 +3,27 @@ const navMobile = document.querySelector('.nav__mobile');
 const navList__container = document.querySelector('.list__container');
 const navLinkArr = document.querySelectorAll('.nav__link');
 //nav animation onscroll
+function navAnimation() {
 
-window.addEventListener('scroll', function(e) {
-    if (window.scrollY > 80) {
-        nav.classList.add('scrolled');
-    } else{
-        nav.classList.remove('scrolled');
-    }
-});
+    window.addEventListener('scroll', function(e) {
+        if (window.scrollY > 80) {
+            nav.classList.add('scrolled');
+        } else{
+            nav.classList.remove('scrolled');
+        }
+    });
 
 //add hover class to rwd menu
-navMobile.addEventListener('mouseover', () => {
+    navMobile.addEventListener('mouseover', () => {
 
-    navList__container.classList.add('list__container--hover');
-    navLinkArr.forEach((element) => {
-        element.addEventListener('click', () => {
-            navList__container.classList.remove('list__container--hover');
+        navList__container.classList.add('list__container--hover');
+        navLinkArr.forEach((element) => {
+            element.addEventListener('click', () => {
+                navList__container.classList.remove('list__container--hover');
+            })
         })
-    })
-});
+    });
+}
 
 
 //adding sections variables to nav__item highlight function
@@ -48,3 +50,5 @@ window.addEventListener('scroll', () => {
         navLinkArr[3].classList.toggle(isActive);
     }
 });
+
+navAnimation();
