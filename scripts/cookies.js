87d2f275086cc6dc4a7432008ada20bd48940224
rednaxela700 <ohleg@windowslive.com>
2019-10-30 23:51:49 +0100

    more changes in cookie.js

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,43 +1,47 @@
-const 
-    cookieInfo = document.getElementById('cookies'),
-    cookieBtn = document.getElementById('cookies__submit');
-let cookieItem = document.cookie;
+function cookieQuest() {
+    const
+        cookieInfo = document.getElementById('cookies'),
+        cookieBtn = document.getElementById('cookies__submit');
+    let cookieItem = document.cookie;
 
-const setMyCookie = () => {
-    cookieItem ="name=user;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=Lax";
-};
+    const setMyCookie = () => {
+        cookieItem ="name=user;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=Lax";
+    };
 
 
-function getCookie(cname) {
-    const name = cname + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-        c = c.substring(1);
+    function getCookie(cname) {
+        const name = cname + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
         }
-        if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-        }
+        return "";
     }
-    return "";
+
+    const popUp = () => {
+        cookieInfo.style.display = 'block';
+        cookieBtn.addEventListener('click', popHide);
+    };
+
+    const popHide = () => {
+        cookieInfo.style.display = 'none';
+        setMyCookie();
+    };
+
+    const cookiePop = () => {
+        if (getCookie('name') === ''){
+            setTimeout(() => popUp(), 1000);
+        }
+    };
+
+    cookiePop();
 }
-
-const popUp = () => {
-    cookieInfo.style.display = 'block';
-    cookieBtn.addEventListener('click', popHide);
-};
-
-const popHide = () => {
-    cookieInfo.style.display = 'none';
-    setMyCookie();
-};
-
-const cookieQuest = () => {
-    if (getCookie('name') === ''){
-        setTimeout(() => popUp(), 1000);
-    }
-};
 
 cookieQuest();
