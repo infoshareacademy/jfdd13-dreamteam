94ad227e696c41d6d8dbac56b52c61a0ab5795cc
rednaxela700 <ohleg@windowslive.com>
2019-10-30 23:37:20 +0100

    deleted unused function, set comparison to strict in conditional statements changed some parts of syntax to ES6

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,29 +1,23 @@
 const 
     cookieInfo = document.getElementById('cookies'),
-    cookieBtn = document.getElementById('cookies__submit'),
-    cookie = document.cookie;
+    cookieBtn = document.getElementById('cookies__submit');
+let cookieItem = document.cookie;
 
 const setMyCookie = () => {
-    document.cookie ="name=user;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=Lax";
-}
-    
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
+    cookieItem ="name=user;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=Lax";
+};
+
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
         c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
         return c.substring(name.length, c.length);
         }
     }
@@ -33,17 +27,17 @@ function getCookie(cname) {
 const popUp = () => {
     cookieInfo.style.display = 'block';
     cookieBtn.addEventListener('click', popHide);
-}
+};
 
 const popHide = () => {
     cookieInfo.style.display = 'none';
     setMyCookie();
-}
+};
 
 const cookieQuest = () => {
     if (getCookie('name') === ''){
         setTimeout(() => popUp(), 1000);
     }
-}
+};
 
 cookieQuest();
