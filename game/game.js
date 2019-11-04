965c2e4213889cbfeddaf36daedfc069b6027828
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-04 09:18:10 +0100

    game built in DOM - take1

--- game/game.js
+++ game/game.js
@@ -1,4 +1,3 @@
-
 class Player {
   constructor(x, y) {
     this._x = x;
@@ -32,7 +31,6 @@ class Move {
     PlaneDom.style.left = PlaneJS.x + "px";
     PlaneDom.style.right = PlaneJS.x + "px";
     PlaneDom.style.top = PlaneJS.y + "px";
-    // console.log(PlaneJS);
   }
 
   static KeySupport(PlaneJS, event) {
@@ -63,10 +61,9 @@ class Move {
       PlaneJS.x === 0 ||
       PlaneJS.x === 880 ||
       PlaneJS.y === 0 ||
-      PlaneJS.y === 580
+      PlaneJS.y === 450
     ) {
       document.querySelector(".board").style.background = "crimson";
-      document.querySelector(".board").style.border = "5px solid black";
       setTimeout(eyeBlink, 150);
       function eyeBlink() {
         alert("GAME OVER! Sorry, this is the end of your trip.");
@@ -79,13 +76,14 @@ class Move {
 Move.RenderPlane(PlaneJS);
 document.addEventListener("keydown", event => Move.Collision(PlaneJS, event));
 
-
 const 
   startBtn = document.getElementById('start__btn'),
   startGame = () => {
     startBtn.style.display = 'none';
     event.preventDefault();
-    setInterval(() => RenderObstacles.RenderObst(),100);
+    CreateObstacles.CreateObst();
+
+    setInterval(() => RenderObstacles.RenderObst(ObstacleArray, '.board'),100);
     document.addEventListener("keydown", event => Move.KeySupport(PlaneJS, event));
 };
 
@@ -142,53 +140,23 @@ const ObstacleArray = [
   new Obstacle('city2', 640, 390),
   new Obstacle('city3', 760, 390)
 ]
+class CreateObstacles {
+  static CreateObst() {
+    const placeToAdd = document.querySelector('.board');
+    placeToAdd.innerHTML = '';
+    Object.values(ObstacleArray).forEach((name,x,y) => {
+      const newObst = document.createElement('div');
+      newObst.innerHTML = '';
+
+      
+      placeToAdd.appendChild(newObst);
+    })
+    }
+  }
+
 class RenderObstacles {
   static RenderObst() {
-    const Bird = document.querySelector('img');
-    const Bird1 = document.querySelector('.bird1');
-    const Bird2 = document.querySelector('.bird2');
-    const Sky1 = document.querySelector('.cloud1');
-    const Sky2 = document.querySelector('.cloud2');
-    const Sky3 = document.querySelector('.cloud3');
-    const Mount = document.querySelector('.mount');
-    const Tree1 = document.querySelector('.tree1');
-    const Tree2 = document.querySelector('.tree2');
-    const Tree3 = document.querySelector('.tree3');
-    const Tree4 = document.querySelector('.tree4');
-    const City1 = document.querySelector('.city1');
-    const City2 = document.querySelector('.city2');
-    const City3 = document.querySelector('.city3');
-    // ObstacleArray.forEach = (item,i) => {item.leftObst()};
-    Bird.style.left = ObstacleArray[0].x + "px";
-    Bird.style.top = ObstacleArray[0].y + "px";
-    Bird1.style.left = ObstacleArray[1].x + "px";
-    Bird2.style.left = ObstacleArray[2].x + "px";
-    Sky1.style.left = ObstacleArray[3].x + "px";
-    Sky2.style.left = ObstacleArray[4].x + "px";
-    Sky3.style.left = ObstacleArray[5].x + "px";
-    Mount.style.left = ObstacleArray[6].x + "px";
-    Tree1.style.left = ObstacleArray[7].x + "px";
-    Tree2.style.left = ObstacleArray[8].x + "px";
-    Tree3.style.left = ObstacleArray[9].x + "px";
-    Tree4.style.left = ObstacleArray[10].x + "px";
-    City1.style.left = ObstacleArray[11].x + "px";
-    City2.style.left = ObstacleArray[12].x + "px";
-    City3.style.left = ObstacleArray[13].x + "px";
-    ObstacleArray[0].leftBird();
-    ObstacleArray[0].downBird();
-    ObstacleArray[1].leftBird();
-    ObstacleArray[2].leftBird();
-    ObstacleArray[3].leftSky();
-    ObstacleArray[4].leftSky();
-    ObstacleArray[5].leftSky();
-    ObstacleArray[6].leftObst();
-    ObstacleArray[7].leftObst();
-    ObstacleArray[8].leftObst();
-    ObstacleArray[9].leftObst();
-    ObstacleArray[10].leftObst();
-    ObstacleArray[11].leftObst();
-    ObstacleArray[12].leftObst();
-    ObstacleArray[13].leftObst();
+    
   }
 }
 
