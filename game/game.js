d7a9cb4af69d0438b7968fe5211f7fdb3b01362b
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-11 16:34:13 +0100

    create player with KeySupport method

--- game/game.js
+++ game/game.js
@@ -1,6 +1,6 @@
 
 
-let id = 1;
+let id = 0;
 
 const 
   body = document.querySelector('body'),
@@ -11,6 +11,8 @@ const
   instBtn = document.getElementById('inst__btn');
 
 const 
+  playerWidth = '40px',
+  playerHeight = '40px',
   obstWidth = '40px',
   obstHeight = '200px',
   birdWidth = '20px',
@@ -30,8 +32,10 @@ startGame = () => {
   startBtn.style.display = 'none';
   instBtn.style.display = 'none';
   event.preventDefault();
-
+  document.addEventListener("keydown", event => Render.KeySupport(Player, event));
+  
   firstLoop = () => {
+    Render.create(createPlayer('pilot'));    
     Render.create(createBird('ufo'));
     Render.create(createObstacle('dom'));
   };
@@ -67,8 +71,13 @@ class Render {
     child.innerText = el.name;
     child.setAttribute('id', `${el.name}${el.id}`);
 
-
-    if (el.type === 'obstacle') {
+    if (el.type === 'player') {
+      child.style.width = playerWidth;
+      child.style.height = playerHeight;
+      child.style.background = `blue`;
+      child.setAttribute('class', `player`);
+     
+    } else if (el.type === 'obstacle') {
       child.style.width = obstWidth;
       child.style.height = obstHeight;
       child.style.background = `grey`;
@@ -101,25 +110,64 @@ class Render {
   static changePosition(domEl) {
     childrenArray.forEach((el,i) =>{
       let x = el.position.x;
+      let y = el.position.y;
       // console.log(`child x= ${x}`);
+      
+      if(el.type==='player'){  
+        el.domEl.style.left = el.position.x + 'px';
+        el.domEl.style.top = el.position.y + 'px';  
+      };
       if(el.type==='obstacle'){
         el.moveObst();
-        el.domEl.style.left = el.position.x + 'px';
+        el.domEl.style.left = x + 'px';
       };
       if(el.type==='bird'){
         el.moveBird();
-        el.domEl.style.left = el.position.x + 'px';
+        el.domEl.style.left = x + 'px';
       };
       if(el.type==='birdZ'){
         el.moveBirdZ();
-        el.domEl.style.left = el.position.x + 'px';
-        el.domEl.style.top = el.position.y + 'px';  
+        el.domEl.style.left = x + 'px';
+        el.domEl.style.top = y + 'px';  
       };
 
     });         
   };
+  static KeySupport(domEl, event) {
+    childrenArray.forEach((el,i) =>{
+      let x = el.position.x;
+      let y = el.position.y;
+  
+      if(el.type==='player'){
+      switch (event.code) {
+        case "ArrowLeft":
+          el.playerLeft();
+          el.domEl.style.left = x + 'px';
+  
+          // Player.changePosition();
+          break;
+        case "ArrowRight":
+          el.playerRight();
+          el.domEl.style.left = x + 'px';
+  
+          break;
+        case "ArrowUp":
+          el.playerUp();
+          el.domEl.style.top = y + 'px';
+  
+          break;
+        case "ArrowDown":
+          el.playerDown();
+          el.domEl.style.top = y + 'px';
+  
+          break;
+        default:
+          return
+        };
+      };
+    });
+  };
 
- 
   static destroy(el) {
 
   }
@@ -138,7 +186,18 @@ class BoardElement {
     this.speed = speed;
     this.type = type;
  }
-
+  playerLeft(){
+  this.position.x -= this.speed;
+  }
+  playerRight(){
+    this.position.x += this.speed;
+  }
+  playerUp(){
+    this.position.y -= this.speed *4;
+  }
+  playerDown(){
+    this.position.y += this.speed *4;
+  }
   moveObst() {
     this.position.x -= this.speed;
   }
@@ -153,36 +212,16 @@ class BoardElement {
 /////////
 
 class Player extends BoardElement {
-  constructor(name, domEl, id, position, speed, type) {
-    super(position, speed)
-    this.name = 'player';
-    this.domEl = document.querySelector('#player');
-    this.id = 0
-    this.type = 'player';
-  }
-
-  static KeySupport(Player, event) {
-    switch (event.code) {
-      case "ArrowLeft":
-        Player.changePosition();
-        break;
-      case "ArrowRight":
-        Player.right();
-        break;
-      case "ArrowUp":
-        Player.up();
-        break;
-      case "ArrowDown":
-        Player.down();
-        break;
-      default:
-        return
-    }
+  constructor(name, domEl, id, position={x:'',y:''}, speed, type) {
+    super(domEl, position);
+    this.name = name;
+    this.id = id;
+    this.position.x = position.x;
+    this.position.y = position.y;
+    this.speed = speed;
+    this.type = type;
   }
 }
-const play = new Player('Andrzej', '', 0, '', '')
-
-// const bird = new Obstacle('Bird', document.getElementById('bird'), 1, {[0] : 100, [1]: 200}, 1)
 
 class Obstacle extends BoardElement {
   constructor(name, domEl, id, position={x:'',y:''}, speed, type) {
@@ -205,7 +244,10 @@ class Bird extends BoardElement {
     this.type = type;
   }
 }
-
+// const play = new Player( 'Andrzej', '', 0, 'speed', 'player')
+createPlayer = (name) => {
+  return new Player(name, '', 0, {x:100,y:200}, speed, 'player');
+};
 createObstacle = (name) => {
   return new Obstacle(name, '', '', {x:700,y:400}, speedObst, 'obstacle');
 };
