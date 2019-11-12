32a0a8d2e626adc3c084cc82e1eba1c596c7d27d
rednaxela700 <ohleg@windowslive.com>
2019-11-12 20:05:36 +0100

    commented out unstable code in authors.js

--- scripts/authors.js
+++ scripts/authors.js
@@ -8,17 +8,17 @@ const authors = () => {
 
         authImgs.forEach(item => {
             item.addEventListener('mouseenter', () => {
-                const newEl = document.createElement('div.card__img--back');
+                // const newEl = document.createElement('div.card__img--back');
                 item.classList.toggle('card__img--transform');
-                newEl.style.background = '#bdbdbd';
-                newEl.style.width = '75px';
-                newEl.style.height = '75px';
-                newEl.style.zIndex = '10';
-                item.appendChild(newEl)
+                // newEl.style.background = '#bdbdbd';
+                // newEl.style.width = '75px';
+                // newEl.style.height = '75px';
+                // newEl.style.zIndex = '10';
+                // item.appendChild(newEl)
             });
             item.addEventListener('mouseleave', () => {
                 item.classList.toggle('card__img--transform');
-                item.removeChild('div');
+                // item.removeChild('div');
             })
         });
 
