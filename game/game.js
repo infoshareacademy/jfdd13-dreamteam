15a060f108e33f33a742f9cf6a9736998f68ca03
rednaxela700 <ohleg@windowslive.com>
2019-11-12 02:53:04 +0100

    added countdown timer to game js

--- game/game.js
+++ game/game.js
@@ -363,5 +363,25 @@ function highScore() {
 
 }
 
+const startTimer = (duration, display) => {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
 
-//tu się kończy
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.innerText = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+};
+
+const countdown = () => {
+    const twoMinutes = 60 * 2,
+        display = document.querySelector('#countdown');
+    startTimer(twoMinutes, display);
+};
