b63c5be1908a1feb3dabb851cffcd8b441d8dc49
rednaxela700 <ohleg@windowslive.com>
2019-11-12 15:17:33 +0100

    stable

--- scripts/authors.js
+++ scripts/authors.js
@@ -13,19 +13,14 @@ authImgs.forEach(item => {
     item.addEventListener('mouseenter', () => {
         const newEl = document.createElement('div.card__img--back')
         item.classList.toggle('card__img--transform')
-        newEl.style.background = '#bdbdbd';
-        newEl.style.width = '75px';
-        newEl.style.height = '75px';
-        newEl.style.zIndex = '10';
-        item.appendChild(newEl)
+
     });
     item.addEventListener('mouseleave', () => {
         item.classList.toggle('card__img--transform')
-        item.removeChild('div')
     })
 });
-
-function authorsModal() {
-    // const modalWindow =
-}
-
+//
+// function authorsModal() {
+//     // const modalWindow =
+// }
+//
