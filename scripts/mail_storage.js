78dc77d825748f2403d2861dbe88d26f8a1c9ed1
rednaxela700 <ohleg@windowslive.com>
2019-11-19 00:24:54 +0100

    changed all f to arrow functions in mail_storage.js

--- scripts/mail_storage.js
+++ scripts/mail_storage.js
@@ -4,36 +4,35 @@ const mailStorage = () => {
         checkbox = document.querySelector('input[type="checkbox"]'),
         btn = document.querySelector('button'),
         submitMail = document.querySelector("#form__submit"),
-        thankYou = document.querySelector("#thankYou");
+        thankYou = document.querySelector("#thankYou"),
 
-    function addMail() {
-        if (validateMail()) {
-            localStorage.setItem("email", inputMail.value);
-            thankYou.classList.add('form__red--no');
-            thankYou.innerText = 'Dziękujemy! Zapewniamy o bezpieczeństwie Państwa danych.';
-            // popGame()
-        } else {
-            thankYou.classList.add('form__red');
-            thankYou.innerText = 'Prosimy o podanie adresu e-mail.';
-        }
+        validateMail = () => {
+            const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(inputMail.value).toLowerCase());
 
-    }
+        },
+        addMail = () => {
+            if (validateMail()) {
+                localStorage.setItem("email", inputMail.value);
+                thankYou.classList.add('form__red--no');
+                thankYou.innerText = 'Dziękujemy! Zapewniamy o bezpieczeństwie Państwa danych.';
+                // popGame()
+            } else {
+                thankYou.classList.add('form__red');
+                thankYou.innerText = 'Prosimy o podanie adresu e-mail.';
+            }
 
-    function givePerm() {
-        event.preventDefault();
-        if (checkbox.checked) {
-            addMail();
-        } else {
-            thankYou.classList.add('form__red');
-            thankYou.innerText = 'Prosimy o wyrażenie zgody na przetwarzanie danych osobowych.';
-        }
-    }
+        },
 
-    function validateMail() {
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(inputMail.value).toLowerCase());
-
-    }
+        givePerm = () => {
+            event.preventDefault();
+            if (checkbox.checked) {
+                addMail();
+            } else {
+                thankYou.classList.add('form__red');
+                thankYou.innerText = 'Prosimy o wyrażenie zgody na przetwarzanie danych osobowych.';
+            }
+        };
 
     submitMail.addEventListener("click", givePerm);
 };
