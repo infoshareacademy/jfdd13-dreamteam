29d97a9f581f9a673e631e34176f33ce3e5f0c4f
rednaxela700 <ohleg@windowslive.com>
2019-11-19 01:25:25 +0100

    added popGame function and popGameModal

--- scripts/mail_storage.js
+++ scripts/mail_storage.js
@@ -5,25 +5,40 @@ const mailStorage = () => {
         btn = document.querySelector('button'),
         submitMail = document.querySelector("#form__submit"),
         thankYou = document.querySelector("#thankYou"),
+        popGameModal = () => {
+        const btnGame = document.getElementById('#gamebtn__game--redirect')
+        }
+        popGame = () => {
+            console.log('popGame init log')
+            //hide wrapper overflow on modal pop
+            const wrapper = document.querySelector('.wrapper');
+            wrapper.style.overflow = 'hidden';
 
+
+            const modal = document.createElement('div')
+            const modalHeight = 300;
+            const modalWidth = 400;
+            modal.style.top = this.scrollY + window.pageYOffset / 2 - modalHeight + 'px';
+            modal.style.left = document.querySelector('.wrapper').offsetWidth / 2 - modalWitdh + 'px'
+
+
+        },
         validateMail = () => {
             const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(String(inputMail.value).toLowerCase());
-
         },
         addMail = () => {
             if (validateMail()) {
                 localStorage.setItem("email", inputMail.value);
                 thankYou.classList.add('form__red--no');
                 thankYou.innerText = 'Dziękujemy! Zapewniamy o bezpieczeństwie Państwa danych.';
-                // popGame()
+                popGame()
             } else {
                 thankYou.classList.add('form__red');
                 thankYou.innerText = 'Prosimy o podanie adresu e-mail.';
             }
 
         },
-
         givePerm = () => {
             event.preventDefault();
             if (checkbox.checked) {
