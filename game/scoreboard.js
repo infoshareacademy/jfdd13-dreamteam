cf529e015e7a7849b931ad6a8c3089b9359cc9e5
rednaxela700 <ohleg@windowslive.com>
2019-12-13 12:08:03 +0100

    added localStorage operations

--- game/scoreboard.js
+++ game/scoreboard.js
@@ -2,5 +2,9 @@ const scoreData = {
   'Andrzej': 300,
   'Paweł': 200
 }
+const getScore = localStorage.getItem('gameScores');
+const setScore = data => localStorage.setItem('gameScores', data)
+const playerScore = 400
+const playerName = 'Dupa'
 
 const setScore = data => localStorage.setItem('gameScores', data)
