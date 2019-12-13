fb0ef394a40b6b16a2c010bcd473d02d9317d2d4
rednaxela700 <ohleg@windowslive.com>
2019-12-13 12:10:41 +0100

    getScore quickfix

--- game/scoreboard.js
+++ game/scoreboard.js
@@ -2,7 +2,7 @@ const scoreData = {
   'Andrzej': 300,
   'Paweł': 200
 }
-const getScore = localStorage.getItem('gameScores');
+const getScore = localStorage.getItem('gameScores') || {};
 const setScore = data => localStorage.setItem('gameScores', data)
 const playerScore = 400
 const playerName = 'Dupa'
