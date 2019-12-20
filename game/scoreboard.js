3ffe63a3cbc844013e8a78cc45fe5caa20140f7b
rednaxela700 <ohleg@windowslive.com>
2019-12-20 19:52:32 +0100

    put back lowestScore in place, formatted document

--- game/scoreboard.js
+++ game/scoreboard.js
@@ -1,64 +1,66 @@
 const scoreData = [
   {
-    name: 'dupa',
+    name: "dupa",
     score: 300
   },
   {
-    name: 'z trupa',
+    name: "z trupa",
     score: 555
   }
-]
-const getScore = JSON.parse(localStorage.getItem('gameScores')) || [];
-const setScore = data => localStorage.setItem('gameScores', JSON.stringify(data))
-const playerScore = 400
-const playerName = 'Dupa'
+];
+const getScore = JSON.parse(localStorage.getItem("gameScores")) || [];
+const setScore = data =>
+  localStorage.setItem("gameScores", JSON.stringify(data));
+const playerScore = 400;
+const playerName = "Dupa";
 
 const addScore = (player, score) => {
   const newScoreData = {
     name: player,
     score: score
-  }
-  getScore.push(newScoreData)
+  };
+  getScore.push(newScoreData);
 
-  return setScore(newScore)
-}
+  return setScore(newScore);
+};
 
 const displayScore = (data, parentEl) => {
-  const listContainer = document.createElement('ul')
+  const listContainer = document.createElement("ul");
   data.map(item => {
-    const domListItem = document.createElement('li')
-    const playerEl = document.createElement('span')
-    playerEl.innerText = item.name
-    domListItem.appendChild(playerEl)
-    const scoreEl = document.createElement('span')
-    scoreEl.innerText = item.score
-    domListItem.appendChild(scoreEl)
-    listContainer.appendChild(domListItem)
-  })
-  parentEl.appendChild(listContainer)
-}
+    const domListItem = document.createElement("li");
+    const playerEl = document.createElement("span");
+    playerEl.innerText = item.name;
+    domListItem.appendChild(playerEl);
+    const scoreEl = document.createElement("span");
+    scoreEl.innerText = item.score;
+    domListItem.appendChild(scoreEl);
+    listContainer.appendChild(domListItem);
+  });
+  parentEl.appendChild(listContainer);
+};
 
-const currentScore = 666
+const currentScore = 666;
 
 const checkScores = arr => {
   if (arr.length) {
-    if(arr.length < 10) {
-      return //retrieve playerName
+    if (arr.length < 10) {
+      return; //retrieve playerName
       //push score using addScore function
     }
-  if (arr.length >= 10 && lowestScore.score < currentScore) {
+    const lowestScore = arr.reduce((prev, cur) => {
+      return prev.score < cur.score ? prev : cur;
+    }, +Infinity);
+    if (arr.length >= 10 && lowestScore.score < currentScore) {
+      // Wez tablice poprzednich wynikow
+      // Dorzuc do niej nowy wynik. Bez znaczenia czy na poczatek czy na koniec (push, unshift)
+      // Posortuj tablice od najwyzszego do najnizszego (sort)
+      // Wez pierwsze 10 elementow (slice)
+      // DONE :slightly_smiling_face:
+    }
 
-    // Wez tablice poprzednich wynikow
-    // Dorzuc do niej nowy wynik. Bez znaczenia czy na poczatek czy na koniec (push, unshift)
-    // Posortuj tablice od najwyzszego do najnizszego (sort)
-    // Wez pierwsze 10 elementow (slice)
-    // DONE :slightly_smiling_face:
-    arr.push(currentScore)
+    arr.push(currentScore);
 
-    
+    console.log(lowestScore);
   }
-
-  console.log(lowestScore)
-  }
-}
+};
 // setScore()
