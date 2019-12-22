07f4ea4e0ea6183b25a8e46cfa5d4f9555eda4b3
rednaxela700 <ohleg@windowslive.com>
2019-12-22 22:05:45 +0100

    formatted scoreboardLogic.js

--- game/scoreboard/scoreboardLogic.js
+++ game/scoreboard/scoreboardLogic.js
@@ -1,44 +1,54 @@
-import {renderScore} from './scoreboardRender'
-import {generateRandomScore, fillScores} from './scoreboardHelpers'
-const btnx = document.getElementById('scoreboardBtn')
-const scoreboard = document.getElementById('scoreboard')
-const playerName = document.getElementById('playerName')
-const resetScore = () => localStorage.setItem("gameScores", JSON.stringify([]));
-const getScore = JSON.parse(localStorage.getItem("gameScores")) || resetScore();
+import { renderScore } from "./scoreboardRender";
+import { generateRandomScore, fillScores } from "./scoreboardHelpers";
+export const scoreboard = () => {
+  const btnx = document.getElementById("scoreboardBtn");
+  const scoreboard = document.getElementById("scoreboard");
+  const playerName = document.getElementById("playerName");
+  const saveScoreBtn = document.getElementById("savescore");
+  const resetScore = () =>
+    localStorage.setItem("gameScores", JSON.stringify([]));
+  const getScore =
+    JSON.parse(localStorage.getItem("gameScores")) || resetScore();
 
-const setScore = data => localStorage.setItem("gameScores", JSON.stringify(data));
-const addScore = (player, score) => {
-  const newScoreData = {
-    name: player,
-    score: score
+  const setScore = data =>
+    localStorage.setItem("gameScores", JSON.stringify(data));
+  const addScore = (player, score) => {
+    const newScoreData = {
+      name: player,
+      score: score
+    };
+    getScore.push(newScoreData);
+    return setScore(getScore);
   };
-  getScore.push(newScoreData);
-  return setScore(getScore);
-};
 
-const sortScores = arr => arr.sort((a, b) => {
-    a.score < b.score ? 1 : -1;
-  ;
-});
+  const sortScores = arr =>
+    arr.sort((a, b) => {
+      a.score < b.score ? 1 : -1;
+    });
 
-const checkScores = arr => {
-  if (arr.length) {
-    if (arr.length <= 10) {
-      const output = sortScores(arr);
-      return renderScore(output, scoreboard)
-    } else {
-      const tenScoresArr = sortScores(arr).slice(0, 10);
-      setScore(tenScoresArr)
-      return renderScore(getScore, scoreboard)
+  const checkScores = arr => {
+    if (arr.length) {
+      if (arr.length <= 10) {
+        const output = sortScores(arr);
+        return renderScore(output, scoreboard);
+      } else {
+        const tenScoresArr = sortScores(arr).slice(0, 10);
+        setScore(tenScoresArr);
+        return renderScore(getScore, scoreboard);
+      }
     }
-  }
+  };
+
+  const handleScores = () => {
+    addScore(playerName, 998);
+    checkScores(getScore);
+  };
+
+  saveScoreBtn.addEventListener("click", () => {
+    handleScores()
+  });
+
+  btnx.addEventListener("click", () => {
+    checkScores(getScore);
+  });
 };
-
-const handleScores = () => {
-  addScore(playerName, 998)
-  checkScores(getScore)
-}
-
-btnx.addEventListener('click' , () => {
-  checkScores(getScore)
-})
