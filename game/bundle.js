53e5291b6b7b32232514ac22b62257a5a1645dc6
rednaxela700 <ohleg@windowslive.com>
2019-12-22 21:20:23 +0100

    rollup output file init

---
+++ game/bundle.js
@@ -0,0 +1,32 @@
+define(function () { 'use strict';
+
+  const instruction = () => {
+    const modal = document.getElementById("instBoxM");
+
+  const btn = document.getElementById("inst__btn");
+
+  const span = document.getElementsByClassName("modal__close")[0];
+  const closeBtn = document.getElementById("close__btn");
+
+  btn.onclick = function() {
+    modal.style.display = "flex";
+  };
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  closeBtn.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+  };
+
+  instruction();
+
+});
