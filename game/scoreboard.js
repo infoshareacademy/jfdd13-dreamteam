c8f956d2ecc4c56d958d25c9055c858e4ea24398
rednaxela700 <ohleg@windowslive.com>
2019-12-22 17:57:44 +0100

    added classes to rendered elements

--- game/scoreboard.js
+++ game/scoreboard.js
@@ -1,12 +1,16 @@
 const renderScore = (data, parentEl) => {
   const listContainer = document.createElement("ul");
+  listContainer.classList.add('scoreboard__container')
   data.map(item => {
     const domListItem = document.createElement("li");
     const playerEl = document.createElement("span");
+    domListItem.classList.add('scoreboard__item')
+    playerEl.classList.add('scoreboard__value')
     playerEl.innerText = item.name;
     domListItem.appendChild(playerEl);
     const scoreEl = document.createElement("span");
     scoreEl.innerText = item.score;
+    scoreEl.classList.add('scoreboard__value')
     domListItem.appendChild(scoreEl);
     listContainer.appendChild(domListItem);
   });
