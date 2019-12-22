f799c583a055a799f37d345052eb0fb8a1f80e31
rednaxela700 <ohleg@windowslive.com>
2019-12-22 22:06:14 +0100

    imported scoreboard to index.js

--- game/bundle.js
+++ game/bundle.js
@@ -30,6 +30,79 @@
   };
   };
 
+  const renderScore = (data, parentEl) => {
+    const listContainer = document.createElement("ul");
+    listContainer.classList.add('scoreboard__container');
+    data.map(item => {
+      const domListItem = document.createElement("li");
+      const playerEl = document.createElement("span");
+      domListItem.classList.add('scoreboard__item');
+      playerEl.classList.add('scoreboard__value');
+      playerEl.innerText = item.name;
+      domListItem.appendChild(playerEl);
+      const scoreEl = document.createElement("span");
+      scoreEl.innerText = item.score;
+      scoreEl.classList.add('scoreboard__value');
+      domListItem.appendChild(scoreEl);
+      listContainer.appendChild(domListItem);
+    });
+    parentEl.appendChild(listContainer);
+  };
+
+  const scoreboard = () => {
+    const btnx = document.getElementById("scoreboardBtn");
+    const scoreboard = document.getElementById("scoreboard");
+    const playerName = document.getElementById("playerName");
+    const saveScoreBtn = document.getElementById("savescore");
+    const resetScore = () =>
+      localStorage.setItem("gameScores", JSON.stringify([]));
+    const getScore =
+      JSON.parse(localStorage.getItem("gameScores")) || resetScore();
+
+    const setScore = data =>
+      localStorage.setItem("gameScores", JSON.stringify(data));
+    const addScore = (player, score) => {
+      const newScoreData = {
+        name: player,
+        score: score
+      };
+      getScore.push(newScoreData);
+      return setScore(getScore);
+    };
+
+    const sortScores = arr =>
+      arr.sort((a, b) => {
+        a.score < b.score ? 1 : -1;
+      });
+
+    const checkScores = arr => {
+      if (arr.length) {
+        if (arr.length <= 10) {
+          const output = sortScores(arr);
+          return renderScore(output, scoreboard);
+        } else {
+          const tenScoresArr = sortScores(arr).slice(0, 10);
+          setScore(tenScoresArr);
+          return renderScore(getScore, scoreboard);
+        }
+      }
+    };
+
+    const handleScores = () => {
+      addScore(playerName, 998);
+      checkScores(getScore);
+    };
+
+    saveScoreBtn.addEventListener("click", () => {
+      handleScores();
+    });
+
+    btnx.addEventListener("click", () => {
+      checkScores(getScore);
+    });
+  };
+
   instruction();
+  scoreboard();
 
 })));
--- game/index.js
+++ game/index.js
@@ -1,3 +1,4 @@
 import {instruction} from './inst-game'
-
+import {scoreboard} from './scoreboard/scoreboardLogic'
 instruction()
+scoreboard()
