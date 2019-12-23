237aacc26a3e78cfe4567c89bfde89763f4e06dd
rednaxela700 <ohleg@windowslive.com>
2019-12-23 08:49:57 +0100

    commented out game.js import as it's not working correctly

--- game/index.js
+++ game/index.js
@@ -1,6 +1,6 @@
 import {instruction} from './inst-game'
 import {scoreboard} from './scoreboard/scoreboardLogic'
-import {game} from './game'
+// import {game} from './game'
 instruction()
 scoreboard()
 game()
