3d072f55d8276af4b6bd915141c77aaf68a71a30
rednaxela700 <ohleg@windowslive.com>
2019-12-27 14:17:12 +0100

    missing semicolon

--- game/js/game/gameIndex.js
+++ game/js/game/gameIndex.js
@@ -3,4 +3,4 @@ import {boardButtons, backToMenu, game} from './game'
 export const gameIndex = () => {
 boardButtons[1].addEventListener("click", game);
 boardButtons[0].addEventListener("click", backToMenu);
-}
+};
