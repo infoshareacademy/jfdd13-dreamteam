517b4a389df2aee853e83e8ea1fcf1b3cd50cadc
rednaxela700 <ohleg@windowslive.com>
2019-12-28 14:58:50 +0100

    added semicolons in index.js

--- game/js/index.js
+++ game/js/index.js
@@ -1,6 +1,6 @@
 import {instruction} from './game/inst-game'
 import {scoreboard} from './scoreboard/scoreboardLogic'
 import {gameIndex} from './game/gameIndex'
-instruction()
-scoreboard()
-gameIndex()
+instruction();
+scoreboard();
+gameIndex();
