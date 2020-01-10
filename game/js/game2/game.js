a4c1275d201b890ba4fd772e0fe4d07586b10b4f
rednaxela700 <ohleg@windowslive.com>
2020-01-10 23:12:00 +0100

    fixed generateBirdY position

--- game/js/game2/game.js
+++ game/js/game2/game.js
@@ -127,7 +127,7 @@ class Game {
     const createObstacle = creationName => {
       //generateBirdY is possible to export
       const generateBirdY = () => {
-        const randPositions = [0, 50, 150, 200, 250, 280, 350, 600];
+        const randPositions = [0, 50, 150, 200, 250, 280, 350, 400];
         const getPosition = Math.round(
           Math.random() * randPositions.length - 1
         ); //generate random arr index
